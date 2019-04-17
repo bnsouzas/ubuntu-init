33e45e28abfabeda10381057c82e65b1e1e4169c
Bruno Neves <bnsouzas@gmail.com>
2019-04-17 09:23:51 -0300

    Update vscode snap

--- install/99-apps.sh
+++ install/99-apps.sh
@@ -3,7 +3,7 @@
 wget -O - https://dbeaver.io/debs/dbeaver.gpg.key | sudo apt-key add -
 echo "deb https://dbeaver.io/debs/dbeaver-ce /" | sudo tee /etc/apt/sources.list.d/dbeaver.list
 apt-get update
-snap install vscode --classic
+snap install code --classic
 snap install postman
 snap install intellij-idea-community --classic
 snap install redis-desktop-manager
