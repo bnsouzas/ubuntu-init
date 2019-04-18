94501d2b74277b08b7a3757ccb8db55cac76eb5e
Bruno Neves Souza Silva <bnsouzas@gmail.com>
2019-04-17 21:01:47 -0300

    Add k8s

--- install/2-python37.sh
+++ install/2-python37.sh
@@ -4,11 +4,11 @@ apt-get install -y build-essential checkinstall libffi-dev libreadline-gplv2-dev
     libbz2-dev zlib1g-dev libncurses5-dev libdb5.3-dev libexpat1-dev liblzma-dev
 
 cd /usr/src
-rm -rf Python-3.7.2
-wget https://www.python.org/ftp/python/3.7.2/Python-3.7.2.tgz
-tar xzf Python-3.7.2.tgz
-rm Python-3.7.2.tgz
-cd Python-3.7.2
+rm -rf Python-3.7.3
+wget https://www.python.org/ftp/python/3.7.3/Python-3.7.3.tgz
+tar xzf Python-3.7.3.tgz
+rm Python-3.7.3.tgz
+cd Python-3.7.3
 ./configure --enable-optimizations --with-lto
 make -j8 build_all
 make -j8 altinstall 
---
+++ install/5-virtualbox.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+sudo add-apt-repository multiverse && sudo apt-get update
+sudo apt install virtualbox -y
